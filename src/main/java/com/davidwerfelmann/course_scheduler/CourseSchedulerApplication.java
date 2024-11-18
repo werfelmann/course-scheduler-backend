a2279d39e027c74951479c17716ec5692817e112
werfelmann <werfelmann@gmail.com>
2024-11-18 17:05:16 -0600

    set up spring boot app

---
+++ src/main/java/com/davidwerfelmann/course_scheduler/CourseSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.davidwerfelmann.course_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidwerfelmann/course_scheduler/CourseSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidwerfelmann.course_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
