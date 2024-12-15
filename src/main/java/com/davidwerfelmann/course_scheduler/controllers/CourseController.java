a70cb72cd23e4b04eb4d510fa1e54ccbadb8a8a3
werfelmann <werfelmann@gmail.com>
2024-12-15 09:43:10 -0600

    CrossOrigin access granted

--- src/main/java/com/davidwerfelmann/course_scheduler/controllers/CourseController.java
+++ src/main/java/com/davidwerfelmann/course_scheduler/controllers/CourseController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/courses")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CourseController {
 
     @Autowired
@@ -35,7 +36,6 @@ public class CourseController {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found.");
         }
-
     }
 
     @PostMapping
